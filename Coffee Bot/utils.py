3764a10a5b16ed2ecd1df780fab8073de4ae8153
Ayoola David <ayooladavid@gmail.com>
2020-05-16 21:10:33 +0100

    Edit coffee Bot pro

--- Coffee Bot/utils.py
+++ Coffee Bot/utils.py
@@ -1,2 +1,5 @@
+#Message reminding the user to make a recognizable selection.
 def print_message():
 	print('I\'m sorry, I did not understand your selection. Please enter the corresponding letter for your response.')
+
+	
