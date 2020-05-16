ed2b20a94312f8f743a29133e65a2e844393efc9
Ayoola David <ayooladavid@gmail.com>
2020-05-16 21:11:55 +0100

    Edit coffee Bot pro

--- Coffee Bot/utils.py
+++ Coffee Bot/utils.py
@@ -2,7 +2,7 @@
 def print_message():
 	print('I\'m sorry, I did not understand your selection. Please enter the corresponding letter for your response.')
 
-	
+#Function to get sizes of coffee orders
 def get_size():
   res = input('What size drink can I get for you? \n[a] Small \n[b] Medium \n[c] Large \n> ')
   
@@ -15,3 +15,17 @@ def get_size():
   else:
     print_message()
     return get_size()
+
+#Function for Latte orders
+def order_latte():
+  res = input('And what kind of milk for your latte? \n[a] 2% milk \n[b] Non-fat milk \n[c] Soy milk \n> ')
+
+  if res == 'a':
+    return 'latte'
+  elif res == 'b':
+    return 'non-fat latte'
+  elif res == 'c':
+    return 'soy latte'
+  else:
+    print_message()
+    return order_latte()
