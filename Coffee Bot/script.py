33f69644d754bd247f355edc0f78b1395b767e96
Ayoola David <ayooladavid@gmail.com>
2020-05-16 20:22:01 +0100

    Add coffee bot

--- Coffee Bot/script.py
+++ Coffee Bot/script.py
@@ -4,3 +4,12 @@ def coffee_bot():
   print('Welcome to the cafe!')
   order_drink = 'y'
   drinks = []
+
+  while order_drink == 'y':
+    size = get_size()  
+    drink_type = get_drink_type()
+
+    drink = '{} {}'.format(size, drink_type)
+    drinks.append(drink)
+    print('Alright, that\'s a {}!'.format(drink))
+      
